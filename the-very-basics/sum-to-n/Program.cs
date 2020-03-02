0203408ebd2ef080d0650a8b16a9ddd394c00388
Keddy Shen <Keddy.Shen@myob.com>
2020-03-02 14:59:52 +1100

    Completed basic sum 1 to n

---
+++ the-very-basics/sum-to-n/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace sum_to_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive integer: ");
+            var n = 0;
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine("Please enter a valid integer");
+            }
+            if (n < 0)
+                Console.WriteLine("Sum: 0");
+            else 
+                Console.WriteLine("Sum: " + n*(n+1)/2);
+        }
+    }
+}
