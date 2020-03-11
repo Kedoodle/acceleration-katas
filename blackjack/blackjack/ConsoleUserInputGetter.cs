404551669f6237986d95ec31b5b9719741ff2ab9
Keddy Shen <kedd.shen@gmail.com>
2020-03-11 13:12:07 +1100

    Able to get user input of 0 or 1

--- blackjack/blackjack/ConsoleUserInputGetter.cs
+++ blackjack/blackjack/ConsoleUserInputGetter.cs
@@ -1,10 +1,19 @@
+using System;
+
 namespace blackjack
 {
     public class ConsoleUserInputGetter : IUserInputGetter
     {
         public Move GetMove()
         {
-            throw new System.NotImplementedException();
+            var input = -1;
+            Console.Write("[0] Hit or [1] Stay?: ");
+            while (!int.TryParse(Console.ReadLine(), out input) && input != 0 && input != 1)
+            {
+                Console.Write("Invalid input! [0] Hit or [1] Stay?: ");
+            }
+
+            return input == 0 ? Move.Hit : Move.Stay;
         }
     }
 
