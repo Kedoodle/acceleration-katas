0697320833d8af909d7276a7be6e2d75186080ee
Keddy Shen <kedd.shen@gmail.com>
2020-03-12 14:19:11 +1100

    Write introduction, input prompts, and magic age details header to console

--- magic-year/magic-year/Program.cs
+++ magic-year/magic-year/Program.cs
@@ -6,7 +6,14 @@ namespace magic_year
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the magic year calculator!\n");
+            
+            Console.Write("Please enter your name: ");
+            Console.Write("Please enter your surname: ");
+            Console.Write("Please enter your annual salary: ");
+            Console.Write("Please enter your work start year: ");
+            
+            Console.WriteLine("\nYour magic age details are:\n");
         }
     }
 }
