81e46b839f7134efdf995ca0a6851afdfa7a1ccc
Keddy Shen <kedd.shen@gmail.com>
2020-03-12 14:20:59 +1100

    Get user input as string

--- magic-year/magic-year/Program.cs
+++ magic-year/magic-year/Program.cs
@@ -9,9 +9,13 @@ namespace magic_year
             Console.WriteLine("Welcome to the magic year calculator!\n");
             
             Console.Write("Please enter your name: ");
+            var firstName = Console.ReadLine();
             Console.Write("Please enter your surname: ");
+            var lastName = Console.ReadLine();
             Console.Write("Please enter your annual salary: ");
+            var annualSalary = Console.ReadLine();
             Console.Write("Please enter your work start year: ");
+            var startYear = Console.ReadLine();
             
             Console.WriteLine("\nYour magic age details are:\n");
         }
