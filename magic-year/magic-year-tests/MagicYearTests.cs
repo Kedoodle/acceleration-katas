6a7d89f4c6a0e6c12f7a91eddaabae50a5a48518
Keddy Shen <kedd.shen@gmail.com>
2020-03-12 14:02:31 +1100

    Test for calculating magic year

--- magic-year/magic-year-tests/MagicYearTests.cs
+++ magic-year/magic-year-tests/MagicYearTests.cs
@@ -6,8 +6,12 @@ namespace magic_year_tests
     public class MagicYearTests
     {
         [Fact]
-        public void Test1()
+        public void MagicYear_StartYear1980_Returns2045()
         {
+            const int expected = 2045;
+            const int startYear = 1980;
+            var actual = Calculator.GetMagicYear(startYear);
+            Assert.Equal(expected, actual);
         }
     }
 }
