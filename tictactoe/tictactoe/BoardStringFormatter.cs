e21713062db5d190b31c35fc1abdb7f4e634f6f7
Keddy Shen <kedd.shen@gmail.com>
2020-03-25 14:55:14 +1100

    Define GetBoardAsString function

--- tictactoe/tictactoe/BoardStringFormatter.cs
+++ tictactoe/tictactoe/BoardStringFormatter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace tictactoe
@@ -8,7 +9,31 @@ namespace tictactoe
         {
             var boardAsString = "";
             
+            for (var y = 0; y < board.GridSize; y++)
+            {
+                for (var x = 0; x < board.GridSize; x++)
+                {
+                    var cell = board.GetCell(x, y);
+                    boardAsString += GetCellAsString(cell);
+                }
+
+                if (y < board.GridSize - 1)
+                {
+                    boardAsString += Environment.NewLine;
+                }
+            }
+            
             return boardAsString;
+        }
+
+        private static IEnumerable<char> GetCellAsString(Cell cell)
+        {
+            return cell.State switch
+            {
+                CellState.X => "X",
+                CellState.O => "O",
+                _ => "."
+            };
         }
     }
 }
