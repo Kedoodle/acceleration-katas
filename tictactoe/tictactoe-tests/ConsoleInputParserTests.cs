f32f965431f04bfa89a3347389f751ce032e84b8
Keddy Shen <kedd.shen@gmail.com>
2020-03-25 16:53:42 +1100

    Add tests for TryParseCoordinates boolean return value

--- tictactoe/tictactoe-tests/ConsoleInputParserTests.cs
+++ tictactoe/tictactoe-tests/ConsoleInputParserTests.cs
@@ -11,10 +11,25 @@ namespace tictactoe_tests
             const int expectedX = 0, expectedY = 1;
 
             const string userInput = "1,2";
-            ConsoleInputParser.TryParseCoordinates(userInput, out var actualX, out var actualY);
+            var successfulParse = ConsoleInputParser.TryParseCoordinates(userInput, out var actualX, out var actualY);
             
+            Assert.True(successfulParse);
             Assert.Equal(expectedX, actualX);
             Assert.Equal(expectedY, actualY);
+        }
+        
+        [Theory]
+        [InlineData("a,b")]
+        [InlineData("a,")]
+        [InlineData(",b")]
+        [InlineData(",")]
+        [InlineData(",,")]
+        [InlineData(" , ")]
+        public void TryParse_InvalidStringCoordinates_ReturnsFalse(string userInput)
+        {
+            var successfulParse = ConsoleInputParser.TryParseCoordinates(userInput, out var actualX, out var actualY);
+            
+            Assert.False(successfulParse);
         }
     }
 }
