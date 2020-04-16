882f5a88b989e24eb8d165d3f9631da3c26d1cb9
Keddy Shen <kedd.shen@gmail.com>
2020-04-16 15:48:44 +1000

    Add GetCoordinate to IGrid interface

--- MarsRover/MarsRover/IGrid.cs
+++ MarsRover/MarsRover/IGrid.cs
@@ -2,6 +2,7 @@ namespace MarsRover
 {
     public interface IGrid
     {
+        ICoordinate GetCoordinate(int x, int y);
         ICoordinate GetAdjacentCoordinateTo(ICoordinate coordinate, Direction direction);
     }
 }
